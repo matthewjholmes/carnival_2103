d30f6fbee2763e8904b5fb927b6a316e860476d7
matthewjholmes <>
2021-04-15 09:33:31 -0600

    attendee empty interest array

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -3,11 +3,27 @@ require './lib/attendee'
 
 RSpec.describe do
 
-  it 'is an instance' do
-    attendee = Attendee.new('Bob', 20)
+    it 'is an instance' do
+      attendee = Attendee.new('Bob', 20)
 
-    expect(attendee).to be_an_instance_of(Attendee)
+      expect(attendee).to be_an_instance_of(Attendee)
+    end
+
+    it 'has attributes' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.name).to eq('Bob')
+      expect(attendee.spending_money).to eq(20)
+
+    end
+
+    it 'has empty interests array' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.interests).to eq([])
+    end
   end
 
-end
-  
+  # attendee.add_interest('Ferris Wheel')
+  #
+  # attendee.interests
