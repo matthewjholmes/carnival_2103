53144070fa3cde013ddac90dcd86b426d2a3bdeb
matthewjholmes <>
2021-04-15 10:53:10 -0600

    carnival attendees by ride interest test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -62,45 +62,20 @@ RSpec.describe do
 
     expect(jeffco_fair.attendees).to eq([bob, sally, johnny])
   end
+
+  it 'lists attendees by ride interest' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    bob = Attendee.new("Bob", 0)
+    bob.add_interest('Ferris Wheel')
+    bob.add_interest('Bumper Cars')
+    sally = Attendee.new('Sally', 20)
+    sally.add_interest('Bumper Cars')
+    johnny = Attendee.new("Johnny", 5)
+    johnny.add_interest('Bumper Cars')
+    jeffco_fair.admit(bob)
+    jeffco_fair.admit(sally)
+    jeffco_fair.admit(johnny)
+
+    expect(jeffco_far.attendees_by_ride_interest).to eq ({:ferris_wheel => [bob], :bumper_cars => [bob, sally, johnny], :scrambler => []})
+  end
 end
-
-def admit(attendee)
-  @attendees << attendee
-end
-
-
-#
-# pry(main)> jeffco_fair.attendees
-# #=> [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab69069e8...>, #<Attendee:0x00007ffab7172d70...>]
-#
-# #Attendees are added even if they don't have enough money for all/any exhibits.
-#
-# pry(main)> jeffco_fair.attendees_by_ride_interest
-# #=> {
-# #    #<Ride:0x00007ffab5a3d3a8...> => [#<Attendee:0x00007ffab70bfce8...>],
-# #    #<Ride:0x00007ffab714bce8...> => [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab69069e8...>, #<Attendee:0x00007ffab7172d70...>],
-# #    #<Ride:0x00007ffab71607d8...> => []
-# #  }
-#
-# pry(main)> jeffco_fair.ticket_lottery_contestants(bumper_cars)
-# #=> [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab7172d70...>]
-#
-# pry(main)> jeffco_fair.draw_lottery_winner(bumper_cars)
-# #=> "Johnny"
-#
-# #In the above method, with this setup, "Johnny" or "Bob" could be returned here - Fun!
-#
-# pry(main)> jeffco_fair.draw_lottery_winner(ferris_wheel)
-# #=> nil
-#
-# #If no contestants are eligible for the lottery, nil is returned.
-#
-# pry(main)> jeffco_fair.announce_lottery_winner(scrambler)
-# #=> "Bob has won the IMAX exhibit"
-#
-# #We realize that, in this setup, Bob is not interested in the scrambler.  However, in your test, the result should match exactly.  You will need to stub the return of `draw_lottery_winner` as the above method should depend on that method :)
-#
-# pry(main)> jeffco_fair.announce_lottery_winner(ferris_wheel)
-# #=> "No winners for this lottery"
-#
-# #If there are no contestants, there are no winners.
